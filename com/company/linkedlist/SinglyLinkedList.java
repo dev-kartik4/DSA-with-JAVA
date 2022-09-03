a63e08336157142d35e4ccc7ab4d131cd8b29fbd
Kartik Telikicherla <76726711+dev-kartik4@users.noreply.github.com>
2022-09-03 11:27:46 +0530

    Update SinglyLinkedList.java

--- com/company/linkedlist/SinglyLinkedList.java
+++ com/company/linkedlist/SinglyLinkedList.java
@@ -124,7 +124,7 @@ public class SinglyLinkedList{
         ListNode current = head;
         ListNode previous = null;
         ListNode next = null;
-        while(current.next != null){
+        while(current != null){
             next = current.next;
             current.next = previous;
             previous = current;
